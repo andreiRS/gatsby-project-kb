50d03fe3f42d118dc58ac6fa6e8bbdce7c9fea30
Razvan Andrei Surdu <razvan.surdu@gmail.com>
2022-03-24 09:14:43 +0200

    Fixed loading images

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -13,9 +13,17 @@ module.exports = {
     {
       resolve: 'gatsby-theme-kb',
       options: {
+        rootNote: 'Home',
         contentPath: path.resolve(__dirname, 'content'),
         wikiLinkLabelTemplate: '[[{{ title }}]]',
-        rootNote: 'Home',
+        ignore: [
+          '**/_layouts/**',
+          '**/.git/**',
+          '**/.github/**',
+          '**/.vscode/**',
+          '**/.cache/**',
+          '**/bak/**',
+        ],        
         getPluginMdx(defaultPluginMdx) {
           defaultPluginMdx.options.gatsbyRemarkPlugins.push({
             resolve: 'gatsby-remark-prismjs',
@@ -23,10 +31,31 @@ module.exports = {
               noInlineHighlight: true,
             },
           })
+
+          // so you can have your relative referenced files served, e.g. '../assets/img.png'.
+          defaultPluginMdx.options.gatsbyRemarkPlugins.push({
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              ignoreFileExtensions: ['md', 'mdx'],
+            },
+          })
+
           return defaultPluginMdx
-        },
+        }
+      },
+    },    
+    {
+      // this plugin makes sure your static files will be served by gatsby,
+      //   but of course you need to reference them by absolute path, e.g. '/assets/img.png'.
+      // if you have multiple directories, copy this plugin section and specify other directory
+      // check https://github.com/csath/gatsby-plugin-copy-files-enhanced to find docs for this plugin
+      resolve: 'gatsby-plugin-copy-files-enhanced',
+      options: {
+        source: path.resolve(__dirname, `../assets`),
+        destination: '/assets',
+        purge: false,
       },
     },
-    'gatsby-plugin-no-sourcemaps',
+    'gatsby-plugin-no-sourcemaps',    
   ],
 }
